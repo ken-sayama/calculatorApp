d130ba9eb9409517b9582754d966db9127066f68
Ken Sayama <sayamaken0402@gmail.com>
2018-05-13 01:31:31 +0900

    first commit

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by Ken Sayama on 2018/05/13.
+//  Copyright © 2018年 Ken Sayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
